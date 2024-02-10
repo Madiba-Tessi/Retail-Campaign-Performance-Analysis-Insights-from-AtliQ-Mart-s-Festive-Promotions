b7ef69f137bfa3e30e165f05ee8b7ab833a1f8bd
Madiba-Tessi <144196289+Madiba-Tessi@users.noreply.github.com>
2024-02-09 23:33:16 -0500

    Business Requests_Resume_Project9.sql

--- Business Requests_Resume_Project9.sql
+++ Business Requests_Resume_Project9.sql
@@ -22,22 +22,33 @@ CONCAT(ROUND(
 	              WHEN promo_type = "33% OFF" THEN ((base_price) * (1 - 0.33)) * (`quantity_sold(after_promo)`)
 				  WHEN promo_type = "50% OFF" THEN ((base_price) / 2) * (`quantity_sold(after_promo)`)
 	              WHEN promo_type = "500 Cashback" THEN ((base_price) - 500) * (`quantity_sold(after_promo)`)
-	              WHEN promo_type = "BOGOF" THEN (base_price) * (`quantity_sold(after_promo)`/ 2) ELSE 0 
+	              WHEN promo_type = "BOGOF" THEN (base_price) * (`quantity_sold(after_promo)`) ELSE 0 
                   END )
 ,1),"M") AS total_revenue_after_promotion 
 FROM dim_campaigns dc JOIN fact_events f
 ON dc.campaign_id = f.campaign_id
 GROUP BY campaign_name;
 
+##New Columns
+ALTER TABLE fact_events 
+ADD COLUMN quantity_sold_AP_updated INT
+
+UPDATE fact_events
+SET 
+quantity_sold_AP_updated = CASE 
+                  WHEN promo_type = "BOGOF" THEN  `quantity_sold(after_promo)`*2
+                  ELSE base_price END
+
+
 #4 Incremental quantity sold by Category
 SELECT Category,
 CONCAT(ROUND(
       (
-      SUM(`quantity_sold(after_promo)` - `quantity_sold(before_promo)`) / SUM(`quantity_sold(after_promo)`)
+      SUM(quantity_sold_AP_updated - `quantity_sold(before_promo)`) / SUM(quantity_sold_AP_updated)
       )*100
 ,1),"%") AS ISU,
 RANK() OVER (ORDER BY 
-ROUND((SUM(`quantity_sold(after_promo)` - `quantity_sold(before_promo)`) / SUM(`quantity_sold(after_promo)`))*100,1)
+ROUND((SUM(quantity_sold_AP_updated - `quantity_sold(before_promo)`) / SUM(quantity_sold_AP_updated))*100,1)
 DESC) AS ranking
 FROM dim_products dp JOIN fact_events f
 ON dp.product_code = f.product_code
@@ -57,7 +68,7 @@ total_revenue_after_promotion = CASE
 	              WHEN promo_type = "33% OFF" THEN ((base_price) * (1 - 0.33)) * (`quantity_sold(after_promo)`)
 				  WHEN promo_type = "50% OFF" THEN ((base_price) / 2) * (`quantity_sold(after_promo)`)
 	              WHEN promo_type = "500 Cashback" THEN ((base_price) - 500) * (`quantity_sold(after_promo)`)
-	              WHEN promo_type = "BOGOF" THEN (base_price) * (`quantity_sold(after_promo)`/ 2) ELSE 0 
+	              WHEN promo_type = "BOGOF" THEN (base_price) * (`quantity_sold(after_promo)`) ELSE 0 
                   END,
 total_revenue_before_promotion = (base_price) * (`quantity_sold(before_promo)`)
 
